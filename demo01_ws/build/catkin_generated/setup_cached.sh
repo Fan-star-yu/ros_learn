15f910af7b2a1428788df3e58a4bf70f4acff3b1
Fan-star-yu <3451962836@qq.com>
2025-02-08 21:28:10 +0800

    ros_learn creat test and git test

---
+++ demo01_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/i/ros_learn/demo01_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-12.1/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/cuda-12.1/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/i/ros_learn/demo01_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/i/ros_learn/demo01_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/i/ros_learn/demo01_ws/src:$ROS_PACKAGE_PATH"
